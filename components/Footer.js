11485a3f02b8403566c718c6e17b625395d84b42
gaura <gauraputu@gmail.com>
2021-10-31 18:47:57 +0700

    adding link to icon

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => (
     <>
         <div style={{height:189}} className="object-position: bottom"></div>
         <div className="text-center bg-primaryRed object-position: bottom">
-        <Image src="/linkedin.png" width={32} height={32} />
+        <a href="https://www.linkedin.com/in/gaura-putu/"><Image src="/linkedin.png" width={32} height={32} /></a>
         {" "}
-        <Image src="/telegram.png" width={32} height={32} />
+        <a href="https://t.me/gauraputu"><Image src="/telegram.png" width={32} height={32} /></a>
         </div>
     </>
 );
