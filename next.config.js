11cbb97c55c2fcb5d7ead06341cf61ffff94a3fd
gaura <gauraputu@gmail.com>
2021-10-31 19:18:51 +0700

    Revert "add image optimization workaround"
    This reverts commit 669dc85e045bb16bfb2350de41948fea9cb6cb87.

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // https://github.com/vercel/next.js/issues/21079
-    // Remove this workaround whenever the issue is fixed
-    images: {
-      loader: 'imgix',
-      path: '/',
-    },
-  }
